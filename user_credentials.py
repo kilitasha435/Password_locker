d8a582e6fff1b0c567a05799275b3ffe2d7d3423
kevin-kili <kilikevin435@gmail.com>
2020-04-17 14:38:37 +0300

    Added the save function in the user credential in the user credentils.py

---
+++ user_credentials.py
@@ -0,0 +1,20 @@
+class User:
+    '''
+    Class that generates instances of user credentials
+    '''
+
+    users_list = [] # Empty list where users will be saved
+
+    def __init__(self, first_name, last_name, password):
+        '''
+        __init__ method that helps us define properties for our objects
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+
+    def save_user(self):
+        '''
+        save_user method that saves user objects in the users_list
+        '''
+        User.users_list.append(self)
